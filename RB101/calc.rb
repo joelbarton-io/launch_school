76e7b56f99f893ea13277e0d3564cb91d6242581
EC2 Default User <ec2-user@ip-172-31-37-206.us-east-2.compute.internal>
2021-10-26 17:27:53 +0000

    refactored calculator program

---
+++ RB101/calc.rb
@@ -0,0 +1,55 @@
+def prompt(message)
+  puts "==> #{message}"
+end 
+
+def valid_num?(number)
+  number.to_i != 0
+end 
+
+loop do
+  
+  int1 = ''
+  int2 = ''
+  operator = ''
+  result = 0
+  
+  loop do
+    prompt("Enter first number: ")
+    int1 = gets.chomp
+    break if valid_num?(int1)
+    prompt("Hmm... that's not a valid number.  Try again!")
+  end
+  
+  loop do
+    prompt("Enter second number: ")
+    int2 = gets.chomp
+    break if valid_num?(int2)
+    prompt("Hmm... that's not a valid number.  Try again!")
+  end 
+  
+  prompt("What type of operation are we performing? 1) addition 2) subtraction 3) multiplication 4) division")
+  
+  loop do 
+    operator = gets.chomp
+    break if %w(1 2 3 4).include?(operator)
+    prompt("Must be 1, 2, 3, 4")
+  end 
+  
+  result = case operator
+  
+  when "1"
+    int1.to_i + int2.to_i
+  when "2"
+    int1.to_i - int2.to_i
+  when "3"
+    int1.to_i * int2.to_i
+  when "4"
+    int1.to_f / int2.to_f
+  end 
+  
+  prompt("The result is #{result}")
+  
+  prompt("Make another calculation? (Y/y)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end 
