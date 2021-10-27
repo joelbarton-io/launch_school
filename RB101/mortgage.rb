ec19a77ff3680a9059df2f9158dfb78198c9cc21
EC2 Default User <ec2-user@ip-172-31-37-206.us-east-2.compute.internal>
2021-10-27 03:27:43 +0000

    worked on mortgage assignment

--- RB101/mortgage.rb
+++ RB101/mortgage.rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def integer?(num)
-  num.to_i.to_s == num 
+  num.to_i.to_s == num
 end
 
 def float?(num)
@@ -22,42 +22,51 @@ def valid_rate?(rate_in_question)
   float?(rate_in_question)
 end
 
-prompt("Welcome to Mortgage Calculator!")
-
-loan_amount = ''
-loan_duration = ''
-interest_rate = ''
-
+prompt("Welcome to the Mortgage Calculator!")
 loop do
-  prompt("What is your loan amount? $:")
-  loan_amount = gets.chomp
-  break if valid_loan?(loan_amount)
-  prompt("Hmm...Invalid entry. Please re-enter your loan amount:")
-end
+  loan_amount = ''
+  loan_duration = ''
+  interest_rate = ''
 
-loop do
-  prompt("What is the duration of your loan? # of years:")
-  loan_duration = gets.chomp
-  break if valid_duration?(loan_duration)
-  prompt("Hmm...Invalid entry. Please re-enter your loan duration:")
-end
-  
-loop do
+  loop do
+    prompt("What is your loan amount? $:")
+    loan_amount = gets.chomp
+    break if valid_loan?(loan_amount)
+    prompt("Hmm...Invalid entry. Please re-enter your loan amount:")
+  end
+
+  loop do
+    prompt("What is the duration of your loan? # of years:")
+    loan_duration = gets.chomp
+    break if valid_duration?(loan_duration)
+    prompt("Hmm...Invalid entry. Please re-enter your loan duration:")
+  end
+
   prompt("An average rate is: 3.081% over 30 years")
-  prompt("What is your interest_rate? ")
-  interest_rate = gets.chomp
-  break if valid_rate?(interest_rate)
-  prompt("Hmm...Invalid entry. Please re-enter your interest rate:")
+
+  loop do
+    prompt("What is your APR (annual percentage rate)?")
+    interest_rate = gets.chomp
+    break if valid_rate?(interest_rate)
+    prompt("Hmm...Invalid entry. Please enter a valid APR:")
+  end
+
+  # format numbers for equation
+  loan = loan_amount.to_f
+  months = loan_duration.to_i * 12
+  rate = (interest_rate.to_f / 100) / 12
+
+  # calculation
+  mortgage = loan * (rate / (1 - (1 + rate)**(-months)))
+
+  # humanize large numbers
+  humanized_loan = loan_amount.chars.to_a.reverse.each_slice(3).map(&:join).join(',').reverse
+
+  prompt("MORTGAGE: $#{mortgage.round(2)}")
+  
+  prompt("Calculate another mortgage? (Y/y)")
+  do_over = gets.chomp
+
+  break prompt("Thank you for using me!  Goodbye!") if do_over.downcase != 'y'
+  prompt("Ready to calculate...")
 end
-
-loan = loan_amount.to_f
-months = loan_duration.to_i
-rate = interest_rate.to_f 
-
-# values = [loan, months, rate]
-
-# p values
-
-mortgage = loan * (rate / (1 - (1 + rate)**(-months*12)))
-
-puts mortgage
