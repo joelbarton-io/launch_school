25d3ace0ec37e7ec78048da6147a5e2488864b05
Joel Barton <joelbarton406@gmail.com>
2021-11-03 01:45:13 +0000

    began hard 1

---
+++ RB101/lesson_3/pp_hard_1.rb
@@ -0,0 +1,21 @@
+# Q 1. 
+# i expect a NAME ERROR since greeting is never initialized 
+
+      # WRONG!!!
+      
+      # normally, my answer is correct.  HOWEVER, when a local
+      # variable is initialized within an if/else/while/for/unless/case
+      # block (EVEN IF that block is never executed), the ruby parser
+      # assigns it the value of nil within its scope
+      # 
+
+# Q 2. 
+# line 1: assigning label to hash object
+# line 2: instantiating label that references value in greetings object
+# line 3: mutating string object referenced by informal_greeting
+# line 4:
+# line 5: prints informal greeting => 'hi there'
+# line 6: prints { a: 'hi there' }
+
+# Q 3. 
+# Q 4. 
