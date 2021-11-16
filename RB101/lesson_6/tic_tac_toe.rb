30dd5dd84fa4265273219a5bf71df3d8effbc2cd
Joel Barton <joelbarton406@gmail.com>
2021-11-16 05:49:09 +0000

    condensed

--- RB101/lesson_6/tic_tac_toe.rb
+++ RB101/lesson_6/tic_tac_toe.rb
@@ -1,12 +1,9 @@
-# constants
-
 WIN_CONDITIONS = {
   1 => [1, 2, 3], 2 => [4, 5, 6], 3 => [7, 8, 9],
   4 => [1, 4, 7], 5 => [2, 5, 8], 6 => [3, 6, 9],
   7 => [1, 5, 9], 8 => [3, 5, 7]
 }
-
-# helper methods
+USER, COMPUTER = "x", "o"
 
 def show_board(brd)
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -15,33 +12,35 @@ def show_board(brd)
   puts "-----+-----+-----"
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
 end
-
-def blank_board
+def initialize_board
   new_board = (1..9).each_with_object({}) do |num, hsh|
     hsh[num] = " "
   end
   new_board
 end
-
-def initialize_instructional_board
+def instructional_board
   new_board = (1..9).each_with_object({}) do |num, hsh|
     hsh[num] = num
   end
   new_board
 end
+def prompt(message) puts ">>> #{message}" end
+def play_again?(answer) answer == "y" end
 
-def prompt(message)
-  puts ">>> #{message}"
+def insert_break
+  0.upto(5) { |el| puts '.'*el }
+  puts ""
 end
 
-def play_again?(answer)
-  answer == "y"
+def continue? # => t/f
+  prompt("ENTER to continue")
+  answer = gets.chomp
+  answer == ""
 end
-
 def generate_computer_move(curr_board, number = 0)
   loop do
     number = (1..9).to_a.sample
-    next if curr_board[number] == "x" || curr_board[number] == "o"
+    next if curr_board[number] == USER || curr_board[number] == COMPUTER
     break number
   end
   number
@@ -51,7 +50,7 @@ def generate_player_move(curr_board, user_input = 0)
   loop do
     prompt("enter a number between 1 and 9")
     user_input = gets.chomp.to_i
-    if curr_board[user_input] == "x" || curr_board[user_input] == "o"
+    if curr_board[user_input] == USER || curr_board[user_input] == COMPUTER
       prompt("box #{user_input} is already populated!")
       next
     end
@@ -60,75 +59,60 @@ def generate_player_move(curr_board, user_input = 0)
   user_input
 end
 
-def win?(board_state, symbol)
+def a_win?(board_state, symbol) # this is my most opaque method, i think. returns => t/f
   same_symbol = Proc.new { |el| board_state[el] == symbol }
-  result = (1...8).any? { |number| WIN_CONDITIONS[number].all?(&same_symbol) }
+  result = (1..8).any? { |number| WIN_CONDITIONS[number].all?(&same_symbol) }
   result
 end
 
-def greet
-  puts ""
-  prompt("      welcome      ")
-  puts ""
-  prompt("this is tic-tac-toe")
-  puts ""
-end
-
-def instructions
-  board = blank_board()
-  prompt("this grid is our board...")
-  puts "..."
-  puts ""
-  show_board(board)
-  puts "..."
-  puts ""
-  prompt("digits correspond to squares...")
-  puts "..."
-  puts ""
-  board = initialize_instructional_board
-  show_board(board)
-  puts "..."
-  puts ""
-  prompt("now that we know how to play...")
-  prompt("press enter to begin playing, or any key to exit")
-  answer = gets.chomp
-  answer
-end
-
-def continue? # t/f
-  prompt("press enter to begin playing, or any key to exit game")
-  answer = gets.chomp
-  answer == ""
-end
-# game
-
 loop do
-  greet()
-  if continue?
-    system("clear")
-    instructions()
-    system("clear")
-  end
+  puts ""
+  prompt("    play    ")
+  puts ""
+  prompt(" tic-tac-toe")
+  insert_break()
+  system("clear") if continue?()
+
+  board = initialize_board()
+  prompt("this here grid is our board...")
+  puts ""
+  show_board(board)
+  insert_break()
+  prompt("just a simple 3 x 3 grid...")
+  puts ""
+  system("clear") if continue?()
+
+  prompt("each DIGIT corresponds to a SQUARE...")
+  puts ""
+  board = instructional_board
+  show_board(board)
+  insert_break()
+  system("clear") if continue?()
+
   rounds_played = 0
+  prompt("Care to play?")
+  insert_break()
+  next system("clear") unless continue?()
+
 
   loop do
-    board = blank_board()
+    system("clear")
+    board = initialize_board()
 
     loop do
       player_move = generate_player_move(board)
-      board[player_move] = "x"
+      board[player_move] = USER
 
-      break prompt("user wins!") if win?(board, "x")
-      break prompt("draw!") if board.all? { |_k, v| v == "x" || v == "o" }
+      break prompt("user wins!") if a_win?(board, USER)
+      break prompt("draw!") if board.all? { |_k, v| v == USER || v == COMPUTER }
 
-      # prompt("computer is 'thinking'...")
       computers_move = generate_computer_move(board)
-      board[computers_move] = "o"
+      board[computers_move] = COMPUTER
       prompt("computer chose square #{computers_move}")
       show_board(board)
 
-      break prompt("computer wins!") if win?(board, "o")
-      break prompt("it's a draw!") if board.all? { |_k, v| v == "x" || v == "o" }
+      break prompt("computer wins!") if a_win?(board, COMPUTER)
+      break prompt("it's a draw!") if board.all? { |_k, v| v == USER || v == COMPUTER }
     end
 
     rounds_played += 1
@@ -140,7 +124,5 @@ loop do
     next prompt("queueing up another round...") if play_again?(user_decision)
     break prompt("exiting now...")
   end
-  break
+  break prompt("thank you for playing!")
 end
-puts "..."
-prompt("the game has now exited")
