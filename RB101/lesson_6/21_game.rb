58557eda1710d60b0b7f5616fa8d7dae857dbb35
Joel Barton <joelbarton406@gmail.com>
2021-11-17 23:04:08 +0000

    init commit for the day, keeping up to date

--- RB101/lesson_6/21_game.rb
+++ RB101/lesson_6/21_game.rb
@@ -9,7 +9,7 @@ def make_suit
   ["jack", "queen", "king"].each do |el|
     one_suit[el] = 10
   end
-  one_suit["ace"] = 1   #some_method(curr_total of hand) =>returns either 1 or 11
+  one_suit["ace"] = 11   #some_method(curr_total of hand) =>returns either 1 or 11
   one_suit
 end
 
@@ -28,10 +28,32 @@ def draw!(n, deck, hand = [])   # n cards, working deck, working hand
   hand
 end
 
+def how_many_aces(hand) # => integer
+  ace_count = 0
+
+  hand.each do |card|
+    if card.first == "ace"
+      ace_count += 1
+    end
+  end
+  ace_count
+end
+
+def calculate_total(hand, total = 0)
+  ace_count = how_many_aces(hand)
+
+  hand.each do |card|
+    total += card[1]
+  end
+  ace_count > 1 ? total - (10 * (ace_count - 1)) : total
+end
+
 def display_both_hands(dealer_hand, user_hand)
-  prompt("Dealer has: #{dealer_hand[0][0]} and unknown.")
+  prompt("Dealer has a #{dealer_hand[0][0]} and [REDACTED].")
   puts ""
-  prompt("Your hand: #{user_hand}")
+  puts "..."
+  puts ""
+  prompt("Your hand: #{user_hand}, totalling: #{calculate_total(user_hand)}")
 end
 
 def bust?(hand, total = 0) # t/f
@@ -42,41 +64,69 @@ def bust?(hand, total = 0) # t/f
 end
 
 def player_turn(dealer_hand, user_hand, deck)
+  # system("clear")
+  display_both_hands(dealer_hand, user_hand)
+
   loop do
     prompt("[h]it?...or...[s]tay?")
     case gets.chomp
     when "h"
       user_hand << draw!(1, deck).flatten(1)
-      puts "You drew a #{user_hand.last[0]}"
-      prompt("Your current hand: #{user_hand}")
+      puts "You drew a #{user_hand.last.first}"
+      display_both_hands(dealer_hand, user_hand)
       break prompt("BUST! Dealer wins...") if bust?(user_hand)
     when 's'
       prompt("No more cards for me!")
-      prompt("Passing to dealer...")
-
-      break dealer_turn(dealer_hand, deck)
+      break prompt("Passing to dealer...")
+      # used to invoke dealer_turn here with dealer hand and deck
     end
   end
 end
 
+
+# multiple aces? if true,
+# one needs to be reassigned to count as 1
+
+
+
 def dealer_turn(dealer_hand, deck)
-  # hit until total >= 17
-  loop do
+  dealer_total = calculate_total(dealer_hand)
+
+  until dealer_total >= 17 do
     break prompt("BUST! Player wins!!!") if bust?(dealer_hand)
     dealer_hand << draw!(1, deck).flatten(1)
+    dealer_total = calculate_total(dealer_hand)
   end
+  dealer_hand
 end
+
 
 puts "game on!"
 
+# initalize deck and two-card hands:
 working_deck = initialize_deck()
-
 mine = draw!(2, working_deck)
 dealers = draw!(2, working_deck)
 
-display_both_hands(dealers, mine)
 
+# play game
 player_turn(dealers, mine, working_deck)
 
-def ace_value(total)
-end
+# dealer_turn(dealer_hand, deck)
+
+
+
+
+
+
+# verifying that ace calculation works, it does...
+# ace_deck = []
+# 26.times do
+#   ace_deck.push(['ace', 11])
+#   ace_deck.push(['3', 3])
+# end
+
+
+# ace_hand = draw!(4, ace_deck)
+
+# prompt("hand: #{ace_hand} and total: #{calculate_total(ace_hand)}")
