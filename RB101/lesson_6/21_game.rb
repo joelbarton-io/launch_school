395f3eb1ee2a77003c3f7a6253ad9f8cda78ed3f
Joel Barton <joelbarton406@gmail.com>
2021-11-21 04:21:12 +0000

    made sure git version of program was current

--- RB101/lesson_6/21_game.rb
+++ RB101/lesson_6/21_game.rb
@@ -25,13 +25,13 @@ def display_dealer_hand(dealer_hand)
 end
 
 def display_user_hand(user_hand)
-  prompt("Your hand: #{user_hand}, totalling: #{calculate_total(user_hand)}")
+  prompt("Your hand: #{user_hand}, for a total of #{calculate_total_of(user_hand)}")
   delayed_clear(false, false)
 end
 
 def make_suit
-  one_suit = (2..10).each_with_object({}) do |num, hsh|
-    hsh[num.to_s] = num
+  one_suit = (2..10).each_with_object({}) do |num, suit|
+    suit[num.to_s] = num
   end
   ["Jack", "Queen", "King"].each do |el|
     one_suit[el] = 10
@@ -47,22 +47,22 @@ def initialize_deck(deck = [])
   deck.flatten(1)
 end
 
-def draw!(n, deck, hand = [])
-  n.times do
+def draw!(amount, deck, hand = [])
+  amount.times do
     a_random_index = (0...deck.length).to_a.sample
     hand.push(deck.delete_at(a_random_index))
   end
   hand
 end
 
-def how_many_aces(hand, ace_count = 0) # => integer
+def how_many_aces(hand, ace_count = 0)
   hand.each do |card|
-    ace_count += 1 if card.first == "ace"
+    ace_count += 1 if card.first == "Ace"
   end
   ace_count
 end
 
-def calculate_total(hand)
+def calculate_total_of(hand) # definitely split up into two methods
   ace_count = how_many_aces(hand)
   raw_total = hand.inject(0) { |sum, card| sum + card[1] }
 
@@ -70,8 +70,11 @@ def calculate_total(hand)
   when 0
     raw_total
   when 1
-    raw_total if raw_total <= 21
-    (raw_total -= (10 * ace_count))
+    if raw_total <= 21
+      raw_total
+    else
+      (raw_total -= (10 * ace_count))
+    end
   else
     loop do
       raw_total -= 10
@@ -81,8 +84,11 @@ def calculate_total(hand)
   end
 end
 
-def bust?(hand) # t/f
-  calculate_total(hand) > 21
+# def total_includes_aces(number_of_aces, crude_total)
+# end
+
+def bust?(hand)
+  calculate_total_of(hand) > 21
 end
 
 def player_turn(dealer_hand, user_hand, deck, continue_to_dealer_turn = false)
@@ -90,7 +96,7 @@ def player_turn(dealer_hand, user_hand, deck, continue_to_dealer_turn = false)
   display_user_hand(user_hand)
 
   loop do
-    prompt("HIT? => [h]") # still no argument validation here
+    prompt("HIT? => [h]") # h or s, then hit enter
     prompt("STAY? => [s]")
     h_or_s = gets.chomp
     delayed_clear()
@@ -106,17 +112,18 @@ def player_turn(dealer_hand, user_hand, deck, continue_to_dealer_turn = false)
         break prompt("Dealer W.")
       end
     when 's'
-      prompt("User STAYS at #{calculate_total(user_hand)}.")
+      prompt("User STAYS at #{calculate_total_of(user_hand)}.")
       prompt("Passing to dealer.")
       delayed_clear()
       break continue_to_dealer_turn = true
     end
   end
-  continue_to_dealer_turn ? calculate_total(user_hand) : nil
+  continue_to_dealer_turn ? calculate_total_of(user_hand) : nil
 end
 
 def dealer_turn(dealer_hand, deck, continue_to_compare_totals = true)
-  dealer_total = calculate_total(dealer_hand)
+  dealer_total = calculate_total_of(dealer_hand)
+
   loop do
     if bust?(dealer_hand)
       prompt("BUST! User W!")
@@ -126,16 +133,16 @@ def dealer_turn(dealer_hand, deck, continue_to_compare_totals = true)
       delayed_clear(true, false)
       break dealer_total
     else
-      prompt("Dealer HITS.")
+      prompt("Dealer HITS!")
       delayed_clear(true, false)
       dealer_hand << draw!(1, deck).flatten(1)
-      dealer_total = calculate_total(dealer_hand)
+      dealer_total = calculate_total_of(dealer_hand)
     end
   end
   continue_to_compare_totals ? dealer_total : nil
 end
 
-def declare_a_winner(user_final_score, dealer_final_score)
+def declare_the_winner(user_final_score, dealer_final_score)
   prompt "User score: #{user_final_score}"
   prompt "Dealer score: #{dealer_final_score}"
   prompt "Tie." if user_final_score == dealer_final_score
@@ -143,19 +150,24 @@ def declare_a_winner(user_final_score, dealer_final_score)
   prompt "Dealer W." if user_final_score < dealer_final_score
 end
 
-def play_round(working_deck = initialize_deck()) # deck passed
-  prompt("Hands dealt.")
+def play_one_round(working_deck = initialize_deck()) # deck can be passed to consecutive rounds
+  prompt("Hands are being dealt.")
   delayed_clear(true, false)
-  mine = draw!(2, working_deck)
-  dealers = draw!(2, working_deck)
-  user_final_score = player_turn(dealers, mine, working_deck)
+  my_hand = draw!(2, working_deck)
+  dealers_hand = draw!(2, working_deck)
+
+  user_final_score = player_turn(dealers_hand, my_hand, working_deck)
+
   if user_final_score
     prompt("Dealer must beat user score of #{user_final_score} to win!")
     delayed_clear(true, false)
-    dealer_final_score = dealer_turn(dealers, working_deck)
-    declare_a_winner(user_final_score, dealer_final_score) if dealer_final_score
+    dealer_final_score = dealer_turn(dealers_hand, working_deck)
+
+    if dealer_final_score
+      declare_the_winner(user_final_score, dealer_final_score)
+    end
   end
-  prompt("Round is over.")
+  prompt("Round is now over.")
   delayed_clear(false, false)
   working_deck
 end
@@ -163,20 +175,22 @@ end
 def game
   prompt("Welcome, User!")
   delayed_clear(true, false)
-  prompt("Loading 21")
+  prompt("Loading up 21")
   delayed_clear(true)
-  passed_deck = play_round()
+  passed_deck = play_one_round()
 
   loop do
-    prompt("Play again? (all keys but [n] will queue another round)")
-    run_it_back = gets.chomp
+    prompt("Play again? (all keys except [n] will queue up another round)")
+    play_another = gets.chomp
     delayed_clear(false)
 
-    break if run_it_back == "n"
-    prompt("Remaining cards #{passed_deck.size}")
-    play_round(passed_deck)
+    break if play_another == "n"
+    prompt("Remaining cards in the deck: #{passed_deck.size}")
+    play_one_round(passed_deck)
   end
-  prompt("Exited 21")
+  prompt("Exiting 21")
+  delayed_clear(true, false)
+  prompt("Thank you for playing!")
 end
 
 game()
