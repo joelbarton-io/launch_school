da0bc19000b641bb5dc345e0ffe75bb3a0b590d1
Joel Barton <joelbarton406@gmail.com>
2022-02-05 13:00:21 -0700

    offense/defense

--- RB120/lesson_5/refactor_1_ttt.rb
+++ RB120/lesson_5/refactor_1_ttt.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Player
   attr_reader :unique_mark, :score
 
@@ -20,8 +22,9 @@ class Score
   end
 end
 
-
 class Board
+
+  attr_reader :squares
   WINNING_LINES = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9],
     [1, 4, 7], [2, 5, 8], [3, 6, 9],
@@ -72,14 +75,20 @@ class Board
 
     WINNING_LINES.each do |line|
       if indices.all? { |idx| @squares[line[idx]].mark == TTTGame::HUMAN_MARKER }
-        # increment human win in score board
         return TTTGame::HUMAN_MARKER
       elsif indices.all? { |idx| @squares[line[idx]].mark == TTTGame::COMPUTER_MARKER }
-        # increment human win in score board
         return TTTGame::COMPUTER_MARKER
       end
     end
     nil
+  end
+
+  def find_at_risk_square(line, curr_board, marker)
+    if curr_board.squares.values_at(*line).select { |sqr| sqr.mark == marker }.count == 2
+      curr_board.squares.select{ |k, v| line.include?(k) && v.mark == Square::INITIAL_MARK}.keys.first
+    else
+      nil
+    end
   end
 
   def reset
@@ -108,15 +117,13 @@ class Square
   end
 end
 
-class LeaderBoard
-
-end
 class TTTGame
   attr_reader :board, :human, :computer
 
   HUMAN_MARKER = 'X'.freeze
   COMPUTER_MARKER = 'O'.freeze
   FIRST_TO_MOVE = HUMAN_MARKER
+  MAX_SCORE = 5
 
   def initialize
     @board = Board.new
@@ -137,7 +144,8 @@ class TTTGame
     loop do
       display_board
       play_one_round
-      display_result
+      display_result_and_increment_score
+      break puts "Game over." if max_score_reached?
       break unless play_again?
       reset
       display_play_again_message
@@ -160,14 +168,23 @@ class TTTGame
     end
   end
 
-  def display_result
+  def display_result_and_increment_score                    # this is troublesome
     clear_screen_and_display_board
 
     case board.winning_marker
-    when human.unique_mark    then puts 'You won the round!'
-    when computer.unique_mark then puts 'Computer won the round!'
-    when nil                  then puts 'The board is full!'
+    when human.unique_mark
+      increment_score_of(human)
+      puts 'You won the round!'
+    when computer.unique_mark
+      increment_score_of(computer)
+      puts 'Computer won the round!'
+    when nil
+      puts 'The board is full!'
     end
+  end
+
+  def max_score_reached?
+    human.score.current == MAX_SCORE || computer.score.current == MAX_SCORE
   end
 
   def play_again?
@@ -214,10 +231,6 @@ class TTTGame
     @current_marker == HUMAN_MARKER
   end
 
-  def computer_move
-    board[board.unclaimed_squares.sample] = computer.unique_mark
-  end
-
   def human_move
     puts "Choose a number (#{joinor(board.unclaimed_squares)}): "
     choice = nil
@@ -229,6 +242,33 @@ class TTTGame
       puts 'Invalid choice.'
     end
     board[choice] = human.unique_mark
+  end
+
+=begin
+when the computer moves, it needs to be able to look at the current board state, see if there are any at-risk
+lines and make its move based upon this information.
+=end
+
+  def computer_move
+    square = nil
+
+    Board::WINNING_LINES.each do |line|
+      square = board.find_at_risk_square(line, board, TTTGame::COMPUTER_MARKER)
+      break if square
+    end
+
+    if !square
+      Board::WINNING_LINES.each do |line|
+        square = board.find_at_risk_square(line, board, TTTGame::HUMAN_MARKER)
+        break if square
+      end
+    end
+
+    if !square
+      square = board.unclaimed_squares.sample
+    end
+
+    board[square] = computer.unique_mark
   end
 
   def joinor(left_over_squares, divider = ', ', final_divider = 'or')
@@ -245,6 +285,11 @@ class TTTGame
     player_who_won.score.increment
   end
 
+  def clear_screen_and_display_board
+    board.clear
+    display_board
+  end
+
   def welcome_message
     puts 'Welcome to TTTGame.'
   end
@@ -252,32 +297,32 @@ class TTTGame
   def farewell_message
     puts 'Thanks for playing.'
   end
-
-  def clear_screen_and_display_board
-    board.clear
-    display_board
-  end
 end
 
-game = TTTGame.new
-game.play
-
+TTTGame.new.play
 
 =begin
 - keeping score
   - i decided to ignore the suggestion to avoid using an instance or global variable here.
   Using an instance variable makes a lot of sense in this context.
-  I decided to define a new class Score, with a few attributes
+  I decided to define a new class Score, with a few attributes.
 
- everytime an outcome has been reached (either computer or player have won, we want to update our scoreboard)
+ Everytime an outcome has been reached (either computer or player have won, we want to update our scoreboard)
  Is this a player attribute? This makes more sense than storing it in an instance variable of either of the
  Board or TTTGame classes. This way, Player and LeaderBoard can collaborate.  After a fair bit of sandbox
  experimentation, I managed to establish the relationship I wanted between Score and Player objects.
 
  The next major challenge was deciding where it made the most sense to invoke TTTGame#increment_score_of(player).
- Following the general flow of the program, it wasn't immediately obvious where to place the method.
+ Following the general flow of the program, it wasn't immediately obvious where to place the method. At first
+ I was leaning towards placing TTTGame#increment_score_of(player) within the Board#winning_marker method since
+ it felt like a natural place for this incrementing action to occur. However, that would involve some sort of
+ class hierarchy in order to access TTTGame#increment_score_of(player) from within the Board class so, that was
+ shortlived.  The next spot that made sense was within TTTGame class.
 
  The placement of the current score breakdown felt fairly straightforward: if we're starting a new round,
  tell the human user the current score. The obvious location is to invoke TTTGame#display_score is after the
  TTTGame#display_play_again_message method call.
+
+ The fact that there are two different #reset methods (one defined within TTTGame, one defined within Board) is
+ troublesome/slightly confusing in my opinion.
 =end
