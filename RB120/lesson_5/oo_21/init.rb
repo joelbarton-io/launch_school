95f04cc15b328e2b3c47f124e5604a126b088294
Joel Barton <joelbarton406@gmail.com>
2022-02-08 11:38:31 -0700

    assignment

--- RB120/lesson_5/oo_21/init.rb
+++ RB120/lesson_5/oo_21/init.rb
@@ -2,19 +2,17 @@ require 'pry'
 require 'yaml'
 
 PROMPTS = YAML.load_file('init.yml')
-=begin
-Here is an overview of the game:
-- Both participants are initially dealt 2 cards from a 52-card deck.
-- The player takes the first turn, and can "hit" or "stay".
-- If the player busts, he loses. If he stays, it's the dealer's turn.
-- The dealer must hit until his cards add up to at least 17.
-- If he busts, the player wins. If both player and dealer stays, then the highest total wins.
-- If both totals are equal, then it's a tie, and nobody wins.
-=end
 
-module Displayable
+module UserExp
+  def wipe
+    system 'clear'
+  end
 
+  def delay(duration = 2)
+    sleep(duration)
+  end
 end
+
 module Talkable
   def talk(text)
     puts text
@@ -24,40 +22,76 @@ end
 
 class Participant
   include Talkable
+
   attr_reader :name
+  attr_accessor :busted
 
   def initialize
     @hand = []
+    @busted = false
   end
 
   def busted?
+    total > 21
   end
 
-  def hit
+  def draw(drawn_card)
+    hand << drawn_card
   end
 
-  def stay
+  def raw_total
+    current_total = 0
+    hand.each do |card|
+      current_total += card.value
+    end
+    current_total
   end
 
-  def draw(card)
-    hand << card
+  def total
+    raw_total = raw_total()
+    return raw_total if number_of_aces.zero?
+    number_of_aces.times do
+      break if raw_total <= 21
+      raw_total -= 11
+    end
+    raw_total
+  end
+
+  def ==(other)
+    total == other.total
+  end
+
+  def >(other)
+    total > other.total
+  end
+
+  def <(other)
+    total < other.total
   end
 
   private
 
   attr_reader :hand
+
+  def number_of_aces
+    hand.select do |card|
+      card.value == :Ace
+    end.size
+  end
 end
 
 class Player < Participant
-
   def initialize
     @name = retrieve_name()
     super()
   end
 
   def show
-    puts "#{name}'s hand contains: "
-    puts hand
+    talk(PROMPTS['your_hand'])
+    hand.each do |card|
+      puts card
+    end
+    nil
   end
 
   private
@@ -68,7 +102,7 @@ class Player < Participant
       talk(PROMPTS['request_name'])
       name = gets.chomp.capitalize
       talk(PROMPTS['confirm_name'])
-      next unless gets.chomp.downcase == 'y'
+      next wipe() unless gets.chomp.downcase == 'y'
       break
     end
     name.freeze
@@ -76,50 +110,50 @@ class Player < Participant
 end
 
 class Dealer < Participant
+  RANDOM_NAME = ['Beep', 'Boop', 'Bingbong', 'Lawrence']
 
   def initialize
     @name = retrieve_name()
+    @busted = false
     super()
   end
 
   def show
-    puts "#{name} shows: "
+    puts "#{name} shows:"
+    puts ''
     puts hand.first
   end
 
   private
 
   def retrieve_name
-    ['Beep', 'Boop', 'Bingbong', 'Lawrence'].sample.freeze
+    RANDOM_NAME.sample
   end
 end
 
-
 class Deck
-  attr_reader :deck
+  attr_reader :current
 
   def initialize
-    @deck = create_deck()
+    @current = create
   end
 
   def deal!
-    deck.pop
+    current.pop
   end
 
   private
 
-  def create_deck()
+  def create
     make_deck.shuffle
   end
 
   def make_deck
-    deck = []
-    Card::SUIT.each do |suit|
+    Card::SUIT.each_with_object([]) do |suit, deck|
       Card::FACE.each do |face|
         deck << Card.new(suit, face.first, face.last)
       end
     end
-    deck
   end
 
   def to_s
@@ -128,21 +162,23 @@ class Deck
 end
 
 class Card
+  attr_reader :value, :face
+
   SUIT = %w(Diamonds Hearts Clubs Spades)
-  FACE = {Ace:    11,
-          Two:    2,
-          Three:  3,
-          Four:   4,
-          Five:   5,
-          Six:    6,
-          Seven:  7,
-          Eight:  8,
-          Nine:   9,
-          Ten:    10,
-          Jack:   10,
-          Queen:  10,
-          King:   10
-         }
+  FACE = { Ace: 11,
+           Two: 2,
+           Three: 3,
+           Four: 4,
+           Five: 5,
+           Six: 6,
+           Seven: 7,
+           Eight: 8,
+           Nine: 9,
+           Ten: 10,
+           Jack: 10,
+           Queen: 10,
+           King: 10 }
+
   def initialize(suit, face, value)
     @suit = suit
     @face = face
@@ -150,37 +186,140 @@ class Card
   end
 
   def to_s
-    "#The #{@face} of #{@suit}"
+    "#{@face} of #{@suit}"
   end
-
 end
 
 class Game
-  attr_reader :current, :player, :dealer
+  include Talkable, UserExp
 
   def initialize
-    @current = Deck.new
+    @deck = Deck.new
     @player = Player.new
     @dealer = Dealer.new
   end
 
-  def start
-    deal_cards_to_both_players(2)
-    binding.pry
-    show_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+  def play
+    welcome()
+    main_game()
+    farewell()
   end
 
   private
 
-  def deal_cards_to_both_players(count = 1) # why is this method in the Game class?
-    count.times do
-      player.draw(current.deal!)
-      dealer.draw(current.deal!)
+  attr_reader :deck, :player, :dealer
+
+  def main_game
+    setup()
+    show_initial_cards()
+    player_takes_turn()
+    return show_result() if player.busted
+
+    wipe()
+    dealer_takes_turn()
+    show_result()
+  end
+
+  def setup
+    2.times do
+      player_draw_from_deck()
+      dealer_draw_from_deck()
     end
+    delay()
+    wipe()
+  end
+
+  def show_initial_cards
+    player.show
+    talk(PROMPTS['line'])
+    dealer.show
+    talk(PROMPTS['line'])
+  end
+
+  def player_takes_turn
+    loop do
+      talk("Your current total: #{player.total}")
+      break unless player_hits?
+      talk(PROMPTS['hit'])
+      dealer.show
+      player_draw_from_deck()
+      break player.busted = true if player_busted?
+    end
+  end
+
+  def dealer_takes_turn
+    talk(PROMPTS['to_dealer'])
+    delay()
+    loop do
+      break if dealer.total >= 17
+      dealer_draw_from_deck()
+    end
+    dealer.busted = true if dealer_busted?
+  end
+
+  def show_result
+    if player.busted
+      puts "#{player.name} busted so #{dealer.name} wins."
+    elsif dealer.busted
+      puts "#{dealer.name} busted so #{player.name} wins."
+    else
+      compare_totals()
+    end
+  end
+
+  def compare_totals
+    if player == dealer
+      puts 'Tie'
+    elsif player < dealer
+      puts "#{dealer.name} won with #{dealer.total}"
+    elsif player > dealer
+      puts "#{player.name} won with #{player.total}"
+    end
+  end
+
+  def dealer_busted?
+    dealer.busted?
+  end
+
+  def player_busted?
+    player.busted?
+  end
+
+  def player_hits?
+    answer = nil
+    loop do
+      talk(PROMPTS['hit_or_stay'])
+      answer = gets.chomp.downcase
+      break if ['h', 's'].include?(answer)
+      talk(PROMPTS['invalid_h_s'])
+      delay()
+      wipe()
+      talk("Your current total: #{player.total}")
+    end
+    answer == 'h'
+  end
+
+  def player_draw_from_deck
+    card_drawn = deck.deal!
+    player.draw(card_drawn)
+    puts "#{player.name} drew the #{card_drawn}"
+    delay()
+  end
+
+  def dealer_draw_from_deck
+    card_drawn = deck.deal!
+    dealer.draw(card_drawn)
+  end
+
+  def welcome
+    talk(PROMPTS['welcome'])
+    delay()
+    wipe()
+  end
+
+  def farewell
+    talk(PROMPTS['farewell'])
   end
 end
 
-Game.new.start
+Game.new.play
